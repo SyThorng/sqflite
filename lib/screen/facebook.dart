9f3edda5f9c2963d36cc6ab2cbc9e7e32ea93978
“SyThorng” <sunsythorng9999@gmail.com>
2022-12-02 15:58:07 +0700

    t

--- lib/screen/facebook.dart
+++ lib/screen/facebook.dart
@@ -1,11 +1,126 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:lottie/lottie.dart';
 
-class fb extends StatelessWidget {
+class fb extends StatefulWidget {
   const fb({super.key});
+
+  @override
+  State<fb> createState() => _fbState();
+}
+
+class _fbState extends State<fb> {
+  File? fileImage;
+
+  Future openCamera() async {
+    final file = await ImagePicker().pickImage(source: ImageSource.camera);
+    setState(() {
+      fileImage = File(file!.path);
+    });
+  }
+
+  Future getImageFromGallary() async {
+    final file = await ImagePicker().pickImage(source: ImageSource.gallery);
+    setState(() {
+      fileImage = File(file!.path);
+    });
+  }
+
+  void Bottomsheet(context) {
+    showModalBottomSheet(
+      backgroundColor: Colors.grey,
+      context: context,
+      builder: (context) => Container(
+        height: 600,
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          ),
+          color: Colors.grey,
+        ),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Icon(
+                      Icons.person,
+                      size: 30,
+                    ),
+                  ),
+                ),
+                Text('Admin'),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    margin: EdgeInsets.only(left: 100),
+                    width: 60,
+                    child: MaterialButton(
+                      child: Icon(
+                        Icons.camera_alt,
+                        color: Colors.blue,
+                      ),
+                      onPressed: () {},
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: 60,
+                    child: MaterialButton(
+                      child: Icon(
+                        Icons.photo,
+                        color: Colors.blue,
+                      ),
+                      onPressed: () {
+                        getImageFromGallary();
+                      },
+                      color: Colors.white,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: MaterialButton(
+                        color: Colors.white,
+                        onPressed: () {},
+                        child: Row(
+                          children: [Text('Post'), Icon(Icons.arrow_right)],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -63,74 +178,4 @@ class fb extends StatelessWidget {
       ),
     );
   }
-}
-
-void Bottomsheet(context) {
-  showModalBottomSheet(
-    backgroundColor: Colors.grey,
-    context: context,
-    builder: (context) => Container(
-      height: 600,
-      decoration: const BoxDecoration(
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(20),
-          topRight: Radius.circular(20),
-        ),
-        color: Colors.grey,
-      ),
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  width: 50,
-                  height: 50,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    shape: BoxShape.circle,
-                  ),
-                  child: Icon(
-                    Icons.person,
-                    size: 30,
-                  ),
-                ),
-              ),
-              Text('Admin'),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  margin: EdgeInsets.only(left: 100),
-                  width: 60,
-                  child: MaterialButton(
-                    child: Icon(
-                      Icons.camera_alt,
-                      color: Colors.blue,
-                    ),
-                    onPressed: () {},
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  width: 60,
-                  child: MaterialButton(
-                    child: Icon(
-                      Icons.photo,
-                      color: Colors.blue,
-                    ),
-                    onPressed: () {},
-                    color: Colors.white,
-                  ),
-                ),
-              )
-            ],
-          )
-        ],
-      ),
-    ),
-  );
 }
