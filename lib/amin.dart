d406a487e2dfe9577d232d39a78ff767b18d4965
“SyThorng” <sunsythorng9999@gmail.com>
2023-01-04 09:07:07 +0700

    g

--- lib/amin.dart
+++ lib/amin.dart
@@ -19,32 +19,55 @@ class _adminState extends State<admin> {
     }
   }
 
+  // final Uri smsLaunchUri = Uri(
+  //   scheme: 'sms',
+  //   path: '071 6288406',
+  //   queryParameters: <String, String>{
+  //     'body': Uri.encodeComponent('Example Subject & Symbols are allowed!'),
+  //   },
+  // );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Admin'),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(40.0),
-            child: Container(
-              width: double.infinity,
-              height: 300,
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                image: AssetImage('assets/J.JPG'),
-              )),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(100.0),
+              child: Container(
+                width: double.infinity,
+                height: 300,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                  image: AssetImage('assets/J.JPG'),
+                )),
+              ),
             ),
-          ),
-          Text(
-            'Admin : Sythorng',
-            style:
-                GoogleFonts.aBeeZee(fontSize: 30, fontWeight: FontWeight.bold),
-          ),
-          TextButton(onPressed: _launchUrl, child: Icon(Icons.facebook)),
-        ],
+            Text(
+              'Admin : Sythorng',
+              style: GoogleFonts.aBeeZee(
+                  fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(onPressed: _launchUrl, child: Icon(Icons.facebook)),
+                TextButton(
+                    onPressed: () async {
+                      final phonenumber = '+885 716288406';
+                      final url = 'tel:$phonenumber';
+                      if (await canLaunch(url)) {
+                        await launch(url);
+                      }
+                    },
+                    child: Icon(Icons.call)),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
